47f74cf7317a06b6de3456c52531d7a7d76c2865
yotamitai <yotamitai@gmail.com>
2020-04-28 08:02:58 +0300

    added lqr_yotam.py

--- HW2/lqr_yotam.py
+++ HW2/lqr_yotam.py
@@ -13,8 +13,11 @@ def get_A(cart_pole_env):
     cart_mass = cart_pole_env.masscart
     pole_length = cart_pole_env.length
     dt = cart_pole_env.tau
-
-    return np.matrix([[0]])
+    I = np.identity(4)
+    A_hat = np.array([[0, 1, 0, 0], [0, 0, (pole_mass / cart_mass) * g, 0], [0, 0, 0, 1],
+                      [0, 0, (g / pole_length) * (1 + (pole_mass / cart_mass)), 0]])
+    A = A_hat * dt + I
+    return A
 
 
 def get_B(cart_pole_env):
@@ -28,8 +31,9 @@ def get_B(cart_pole_env):
     cart_mass = cart_pole_env.masscart
     pole_length = cart_pole_env.length
     dt = cart_pole_env.tau
-
-    return np.matrix([[0]])
+    B_hat = np.array([[0], [1 / cart_mass], [0], [1 / (cart_mass * pole_length)]])
+    B = B_hat * dt
+    return B
 
 
 def find_lqr_control_input(cart_pole_env):
@@ -47,14 +51,17 @@ def find_lqr_control_input(cart_pole_env):
     B = get_B(cart_pole_env)
 
     # TODO - Q and R should not be zero, find values that work, hint: all the values can be <= 1.0
-    Q = np.matrix([
+    # the state cost - add larger values where you want there to be more punishment for errors
+    # in this case we want to punish for errors in location and in angle (x[0],x[2])
+    Q = np.array([
+        [1, 0, 0, 0],
         [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
+        [0, 0, 1, 0],
         [0, 0, 0, 0]
     ])
 
-    R = np.matrix([0])
+    # the control cost
+    R = np.array([1])
 
     # TODO - you need to compute these matrices in your solution, but these are not returned.
     Ps = []
@@ -63,6 +70,44 @@ def find_lqr_control_input(cart_pole_env):
     us = []
     xs = [np.expand_dims(cart_pole_env.state, 1)]
     Ks = []
+
+    """LQR"""
+    A_T = A.transpose()
+    B_T = B.transpose()
+    Q_f = Q
+    P_0 = Q_f
+    Ps.append(P_0)
+
+    for i in range(1, cart_pole_env.planning_steps + 1):
+        # Previous P
+        P_minus_1 = Ps[i - 1]
+
+        # calculate K
+        a = np.linalg.multi_dot([B_T, P_minus_1, B])
+        b = np.add(R, a).transpose()
+        K = - np.linalg.multi_dot([b, B_T, P_minus_1, A])
+        # K = - (R + np.linalg.multi_dot([B_T, P_minus_1, B])).transpose() * B_T * P_minus_1 * A
+        # K_T = K.transpose()
+        Ks.append(K)
+
+        # Calculate new P
+        c = np.linalg.multi_dot([A_T, P_minus_1, A])
+        d = np.linalg.multi_dot([A_T, P_minus_1, B, b, B_T, P_minus_1, A])
+        P = np.add(Q, c, -d)
+
+        # pt_3 = np.add(A, B.dot(K))
+        # P = np.add(Q, np.linalg.multi_dot([K_T, R, K]), np.linalg.multi_dot([pt_3.transpose(), P_minus_1, pt_3]))
+        # P = Q + K_T*R*K +(A + B*K).transpose()*P_minus_1*(A + B*K)
+        Ps.append(P)
+
+        # control step u
+        u = K.dot(cart_pole_env.state)
+        u = np.expand_dims(u, 1)
+        us.append(u)
+
+        # state step x
+        x_plus_1 = np.add(A.dot(xs[i - 1]), B.dot(u))
+        xs.append(x_plus_1)
 
     assert len(xs) == cart_pole_env.planning_steps + 1, "if you plan for x states there should be X+1 states here"
     assert len(us) == cart_pole_env.planning_steps, "if you plan for x states there should be X actions here"
