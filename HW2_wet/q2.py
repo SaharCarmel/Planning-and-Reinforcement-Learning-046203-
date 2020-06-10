b7ecb879ee8af56877c5c12013fc346b6ff7c1e3
yotam <yotamitai@gmail.com>
2020-06-10 15:45:23 +0300

    some commits

--- HW2_wet/q2.py
+++ HW2_wet/q2.py
@@ -78,47 +78,49 @@ def PI(initial_policy):
     return new_policy, initial_states_per_iteration
 
 
+
+def TD_0(initial_policy):
+    pass
+
+
 def simulate(s, a):
     cost = reward(s)
     success = 1 if random.random() < Mu[a] else 0
-    if success:
-        temp = list(s)
-        temp.remove(a)
-        next_state = tuple(temp)
-    else:
-        next_state = s
+    next_state = tuple(set(s).difference([a])) if success else s
     return cost, next_state
 
 
-Mu = [0, 0.6, 0.5, 0.3, 0.7, 0.1]
-C = [0, 1, 4, 6, 2, 9]
-S = [(0,)]
-for i in range(5):
-    S += list(combinations([1, 2, 3, 4, 5], i + 1))
-n_VI_iterations = 250
-if __name__ == '__main__':
-    c_mu_policy_value = run_VI(c_mu_pi())
-    max_cost_policy_value = run_VI(max_cost_pi())
+def pt_c():
+    values = run_VI(max_cost_pi())
+    x = np.arange(len(values))
+    fig, ax = plt.subplots()
+    ax.bar(x, list(values.values()), label='Max Cost Policy')
+    ax.set_ylabel('Value')
+    ax.set_title('State Values')
+    ax.set_xticks(x)
+    ax.set_xticklabels(list(values.keys()), rotation='vertical')
+    ax.legend()
+    fig.tight_layout()
+    plt.show()
 
-    simulate((1, 2, 3, 4), 2)
 
-    """PI"""
+def pt_d():
     optimal_policy, initial_state_vals = PI(max_cost_policy_value)
-    optimal_policy_value = run_VI(optimal_policy)
-    # plt.plot(initial_state_vals, '-o', linewidth=2,
-    #          label='Initial state value')
-    # plt.title('Initial state value per Policy Iteration')
-    # plt.ylabel('Initial state value')
-    # plt.xlabel('Iteration')
-    # plt.legend()
-    # plt.show()
+    plt.plot(initial_state_vals, '-o', linewidth=2,
+             label='Initial state value')
+    plt.title('Initial state value per Policy Iteration')
+    plt.ylabel('Initial state value')
+    plt.xlabel('Iteration')
+    plt.legend()
+    plt.show()
 
-    """VI"""
+
+def pt_e():
     x = np.arange(len(optimal_policy_value))
     width = 0.35
     fig, ax = plt.subplots()
-    bar1 = ax.bar(x - width / 2, list(c_mu_policy_value.values()), 0.35, label='C Mu Policy')
-    bar2 = ax.bar(x + width / 2, list(max_cost_policy_value.values()), 0.35, label='Max Cost Policy')
+    ax.bar(x - width / 2, list(c_mu_policy_value.values()), 0.35, label='C Mu Policy')
+    ax.bar(x + width / 2, list(max_cost_policy_value.values()), 0.35, label='Max Cost Policy')
     ax.set_ylabel('Value')
     ax.set_title('State Values')
     ax.set_xticks(x)
@@ -126,3 +128,42 @@ if __name__ == '__main__':
     ax.legend()
     fig.tight_layout()
     plt.show()
+
+
+"""Globals"""
+Mu = [0, 0.6, 0.5, 0.3, 0.7, 0.1]
+C = [0, 1, 4, 6, 2, 9]
+S = [(0,)]
+for i in range(5):
+    S += list(combinations([1, 2, 3, 4, 5], i + 1))
+n_VI_iterations = 250
+
+
+def pt_g():
+    V = {x: 0 for x in S}
+    for i in range(3):
+        pass
+
+if __name__ == '__main__':
+    c_mu_policy_value = run_VI(c_mu_pi())
+    max_cost_policy_value = run_VI(max_cost_pi())
+    optimal_policy, initial_state_vals = PI(max_cost_pi())
+    optimal_policy_value = run_VI(optimal_policy)
+
+    simulate((1, 2, 3, 4), 2)
+    """PART 1"""
+    # pt_c()
+    # pt_d()
+    # pt_e()
+    """PART 2"""
+    pt_g()
+    pt_h()
+    pt_i()
+    pt_j()
+
+    c_mu_policy_value = run_VI(c_mu_pi())
+    max_cost_policy_value = run_VI(max_cost_pi())
+
+    simulate((1, 2, 3, 4), 2)
+
+
